31369a8c93d84f526147904e08ea03281b6a43ba
daniel <lesamessipa@gmail.com>
2021-09-10 10:21:39 +0200

    abort register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -39,7 +39,7 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
-        // abort(403);
+        abort(403);
     }
 
     /**
